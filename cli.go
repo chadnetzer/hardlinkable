7166f379cf2ec2ee4d6bd6f4e7ed5170eb676955
Chad Netzer <chad.netzer@gmail.com>
2018-09-05 20:39:11 -0700

    Clarify Use command text to show options and args

--- cli.go
+++ cli.go
@@ -108,12 +108,13 @@ var MyCLIOptions CLIOptions
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:     "hardlinkable",
+	Use:     "hardlinkable [OPTIONS] dir1 [dir2 ...]",
 	Version: "0.9 alpha - 2018-09-05 (Sep 5 2018)",
 	Short:   "A tool to save space by hardlinking identical files",
 	Long: `A tool to scan directories and report on the space that could be saved by hard
 linking identical files.  It can also perform the linking.`,
 	Args: cobra.MinimumNArgs(1),
+	DisableFlagsInUseLine: true,
 	Run: func(cmd *cobra.Command, args []string) {
 		i, ok := ArgsAreDirs(args)
 		if ok {
