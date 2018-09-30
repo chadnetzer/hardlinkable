e4e03f168b970e3d3df8159fe5dedeab1535b34a
Chad Netzer <chad.netzer@gmail.com>
2018-09-30 04:10:16 -0700

    Revise comments about using foundDirectory in walk

--- walk.go
+++ walk.go
@@ -30,9 +30,7 @@ import (
 
 // Return allowed pathnames through the given channel.
 func matchedPathnames(opts Options, r *Results, dirs []string, files []string) <-chan string {
-	// Options is a copy to prevent being changed during walk.  Also note
-	// that Results.foundDirectory() is only called from inside this
-	// particular goroutine, so is safe from races.
+	// Options is a copy to prevent being changed during walk.
 	out := make(chan string)
 	go func() {
 		defer close(out)
